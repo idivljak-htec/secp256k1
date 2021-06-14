cca0290b4766139828fa101b842e754d9cdcacec
Ivan Divljak <idivljak@execom.eu>
2021-06-14 15:08:21 +0200

    Package updated

--- Package.swift
+++ Package.swift
@@ -5,11 +5,11 @@ import PackageDescription
 let package = Package(
     name: "secp256k1",
     products: [
-        .library(name: "secp256k1", targets: ["secp256k1"]),
+        .library(name: "secp256k1V2", targets: ["secp256k1V2"]),
     ],
     targets: [
         .target(
-            name: "secp256k1",
+            name: "secp256k1V2",
             path: ".",
             sources: ["src/secp256k1.c"],
             publicHeadersPath: "include",
